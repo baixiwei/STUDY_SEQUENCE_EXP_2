3bf6895d93408de7a5a866763ec83366748c5bbf
David William Braithwaite <baixiwei@gmail.com>
2013-08-27 15:03:59 -0400

    Added temporary index.php
    This index.php is a placeholder that informs participants that the
    experiment isn't up yet.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<?php
+// echo "Hello world."
+?>
+
+
+<html>
+
+<head>
+    <title>Mean, Median, and Mode Tutorial</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="welcome">
+            <p>This tutorial will become available on August 30th, 2013.</p>
+        </div>
+    </div>
+</body>
+
+</html>
